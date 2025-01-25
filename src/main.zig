09b518990230f4b140ba224e630b5c0d6ff20bb0
Alexamakans <fougner.alexander@gmail.com>
2025-01-25 17:41:34 +0100

    asdf

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,46 @@
 const std = @import("std");
+const log = std.log;
+const net = std.net;
+const process = std.process;
+
+const arg_error_msg = "missing argument. specify test to run. i.e. 00, 01, ...";
+
+const smoketest = @import("problems/0-smoketest.zig");
+const primetime = @import("problems/1-primetime.zig");
+const meanstoanend = @import("problems/2-meanstoanend.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const args = try process.argsAlloc(gpa.allocator());
+    defer process.argsFree(gpa.allocator(), args);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    if (args.len == 1) {
+        log.err(arg_error_msg, .{});
+        process.exit(1);
+    }
 
-    try bw.flush(); // don't forget to flush!
-}
+    const option = std.fmt.parseInt(u8, args[1], 10) catch {
+        log.err(arg_error_msg, .{});
+        process.exit(1);
+    };
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    switch (option) {
+        0 => {
+            log.info("Running 00 - Smoke Test", .{});
+            try smoketest.main();
+        },
+        1 => {
+            log.info("Running 01 - Prime Time", .{});
+            try primetime.main();
+        },
+        2 => {
+            log.info("Running 02 - Means to an End", .{});
+            try meanstoanend.main();
+        },
+        else => {
+            log.err("test not found, try: 00, 01, ...", .{});
+        },
+    }
 }
